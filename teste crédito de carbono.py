0446fe6f26c2a131a48445643d5db58d6833e21f
FefeAlice <138729400+FefeAlice@users.noreply.github.com>
2023-10-26 11:23:50 -0300

    Update teste crédito de carbono.py

--- teste crédito de carbono.py
+++ teste crédito de carbono.py
@@ -9,8 +9,8 @@ if trans == 1:
         com = int(input(" Qual o combustível que você utiliza no véiculo?. \n 1 - Gasolina \n 2 - Diesel \n 3 - Alcool \n 4- Gás Natural (GNV)\n Escreva aqui qual o combústivel utilizado"))
         if com == 1:
             result = kml*0.82*0.75*3.7
-            if result == 
-            print (f" Sua emissão de CO2 é de:{result}. Tente evitar a emissão de CO2, a cada 1 tonelada evitada, gera um crédito de carbono")
+            if result < 5:
+            print (f" Sua emissão de CO2 é de:{result}. Por conseguir emitir menos de 5 kg de CO2, você acaba ganhando 15% de desconto em suas contas")
 
 if trans == 2:
     esc = int(input("Agora escolha o meio de transporte em que queira calcular o Crédito de Carbono: \n 1 - Ônibus \n 2 - Trem \n Escreva aqui o véiculo desejado: "))
